0c8e7eb337a709db54027fd7b53d75d5dfb772d0
emilio <emilio.gagliardi@gmail.com>
2023-04-03 19:32:11 -0600

    added function to main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def add(x, y):
+    return x + y
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
